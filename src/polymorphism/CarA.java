b4ae1ba1557448b946468387567cac31d3cbe3a4
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-10-26 14:52:38 -0700

    Create CarA.java

---
+++ src/polymorphism/CarA.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class CarA extends Vehicle
+{
+    public CarA()
+    {
+        super();
+    }
+    
+    public CarA( boolean isVehicleOn )
+    {
+        super( isVehicleOn );
+    }
+}
