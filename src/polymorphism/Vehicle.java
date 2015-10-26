7fedb72ce694d9de43970bbb4570efad9642bf82
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-10-26 14:53:22 -0700

    Update Vehicle.java

--- src/polymorphism/Vehicle.java
+++ src/polymorphism/Vehicle.java
@@ -1,1 +1,31 @@
+package polymorphism;
+
+public abstract class Vehicle
+{
+    private boolean isVehicleOn;
+    
+    public Vehicle()
+    {
+        isVehicleOn = false;
+    }
+    
+    public Vehicle( boolean isVehicleOn )
+    {
+        this.isVehicleOn = isVehicleOn;
+    }
+    
+    public void Start()
+    {
+        System.out.println("Starting vehicle");
+        isVehicleOn = true;
+    }
+    
+    public void Drive()
+    {
+        if ( isVehicleOn )
+            System.out.println("Driving Vehicle");
+        else 
+            System.out.println("Start the vehicle first");
+    }  
+}
 
